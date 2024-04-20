97728dd30bdaa2012564ef46ea66bf0736586378
NeilTheSeal <neil.hendren@gmail.com>
2024-04-19 18:46:58 -0600

    Implement coupon model spec

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,4 +3,15 @@ class Coupon < ApplicationRecord
 
   has_many :invoices
   belongs_to :merchant
+
+  enum :discount_type, { percentage: 0, dollar: 1 }
+  enum :status, { deactivated: 0, activated: 1 }
+
+  def display_discount
+    if discount_type == "percentage"
+      "#{discount}%"
+    else
+      "$#{format('%.2f', discount.to_f / 100)}"
+    end
+  end
 end
