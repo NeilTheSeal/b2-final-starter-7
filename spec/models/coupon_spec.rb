4c06eeee8ff6c9e255d6285fc37df3fcb4dedcc9
NeilTheSeal <neil.hendren@gmail.com>
2024-04-23 17:55:48 -0600

    Add model test for counting successful transactions with coupon

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -33,6 +33,46 @@ RSpec.describe Coupon, type: :model do
       status: "activated",
       merchant: @merchant
     )
+
+    @customer = create(:customer)
+
+    @invoice1 = @coupon1.invoices.create!(
+      customer: @customer,
+      status: 1
+    )
+    @invoice2 = @coupon1.invoices.create!(
+      customer: @customer,
+      status: 1
+    )
+    @invoice3 = @coupon1.invoices.create!(
+      customer: @customer,
+      status: 1
+    )
+
+    @transaction1 = Transaction.create!(
+      invoice: @invoice1,
+      credit_card_number: "0000000000000000",
+      credit_card_expiration_date: "01/25",
+      result: 0
+    )
+    @transaction2 = Transaction.create!(
+      invoice: @invoice2,
+      credit_card_number: "0000000000000000",
+      credit_card_expiration_date: "01/25",
+      result: 1
+    )
+    @transaction3 = Transaction.create!(
+      invoice: @invoice2,
+      credit_card_number: "0000000000000000",
+      credit_card_expiration_date: "01/25",
+      result: 0
+    )
+    @transaction3 = Transaction.create!(
+      invoice: @invoice3,
+      credit_card_number: "0000000000000000",
+      credit_card_expiration_date: "01/25",
+      result: 1
+    )
   end
 
   it "displays the discount as a formatted string" do
@@ -79,5 +119,9 @@ RSpec.describe Coupon, type: :model do
     )
     expect(coupon.status).to eq("deactivated")
     expect(coupon.save).to eq(true)
+  end
+
+  it "counts the number of successful transactions with the coupon" do
+    expect(@coupon1.count_uses).to eq(2)
   end
 end
