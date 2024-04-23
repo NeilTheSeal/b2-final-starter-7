04c05a738abf5d1e6d6ab6d9cef979c5a6f836fa
NeilTheSeal <neil.hendren@gmail.com>
2024-04-23 17:54:10 -0600

    Add validation and relation tests for coupon

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,6 +1,18 @@
 require "rails_helper"
 
 RSpec.describe Coupon, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :code }
+    it { should validate_presence_of :discount_type }
+    it { should validate_presence_of :discount }
+    it { should validate_presence_of :status }
+  end
+  describe "relationships" do
+    it { should belong_to :merchant }
+    it { should have_many :invoices }
+  end
+
   before(:each) do
     @merchant = Merchant.create!(name: "Hair Care")
 
