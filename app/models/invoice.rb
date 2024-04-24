3cc8962d60478d12aab992162b77725261d4db98
NeilTheSeal <neil.hendren@gmail.com>
2024-04-23 23:09:07 -0600

    Implement models for invoice coupon calculations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -17,10 +17,27 @@ class Invoice < ApplicationRecord
   def total_revenue
     if coupon.nil?
       subtotal_revenue
-    elsif coupon.discount_type == "percentage"
-      (subtotal_revenue * (100 - coupon.discount) / 100).round(2)
     else
-      [0, subtotal_revenue - coupon.discount].max
+      total_with_valid_coupons + total_without_valid_coupons
     end
+  end
+
+  def total_with_valid_coupons
+    if coupon.discount_type == "percentage"
+      invoice_items.joins(invoice: :coupon, item: :merchant)
+                   .where("coupons.merchant_id = items.merchant_id")
+                   .sum("invoice_items.quantity * invoice_items.unit_price * (100 - discount) / 100")
+    elsif coupon.discount_type == "dollar"
+      total = invoice_items.joins(invoice: :coupon, item: :merchant)
+                           .where("coupons.merchant_id = items.merchant_id")
+                           .sum("invoice_items.unit_price * invoice_items.quantity")
+      [0, total - coupon.discount].max
+    end
+  end
+
+  def total_without_valid_coupons
+    invoice_items.joins(invoice: :coupon, item: :merchant)
+                 .where("coupons.merchant_id != items.merchant_id")
+                 .sum("invoice_items.unit_price * invoice_items.quantity")
   end
 end
