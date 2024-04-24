6a657bfd7eac1fad48189cf5a08aac50411b9a85
NeilTheSeal <neil.hendren@gmail.com>
2024-04-23 23:11:19 -0600

    Add test for total revenue with coupons

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -2,143 +2,136 @@ require "rails_helper"
 
 RSpec.describe "invoices show" do
   before :each do
-    @merchant1 = Merchant.create!(name: "Hair Care")
-    @merchant2 = Merchant.create!(name: "Jewelry")
+    @merchant = Merchant.create!(name: "Hair Care")
 
-    @item_1 = Item.create!(name: "Shampoo",
-                           description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
-    @item_2 = Item.create!(name: "Conditioner",
-                           description: "This makes your hair shiny", unit_price: 8, merchant_id: @merchant1.id)
-    @item_3 = Item.create!(name: "Brush",
-                           description: "This takes out tangles", unit_price: 5, merchant_id: @merchant1.id)
-    @item_4 = Item.create!(name: "Hair tie",
-                           description: "This holds up your hair", unit_price: 1, merchant_id: @merchant1.id)
-    @item_7 = Item.create!(name: "Scrunchie",
-                           description: "This holds up your hair but is bigger", unit_price: 3, merchant_id: @merchant1.id)
-    @item_8 = Item.create!(name: "Butterfly Clip",
-                           description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+    @item1 = Item.create!(
+      name: "Shampoo",
+      description: "This washes your hair",
+      unit_price: 1000,
+      merchant_id: @merchant.id,
+      status: 1
+    )
+    @item2 = Item.create!(
+      name: "Conditioner",
+      description: "This makes your hair shiny",
+      unit_price: 800,
+      merchant_id: @merchant.id
+    )
+    @item3 = Item.create!(
+      name: "Brush",
+      description: "This takes out tangles",
+      unit_price: 500,
+      merchant_id: @merchant.id
+    )
+    @item4 = Item.create!(
+      name: "Thing",
+      description: "It's a thing",
+      unit_price: 1200,
+      merchant_id: @merchant.id
+    )
 
-    @item_5 = Item.create!(name: "Bracelet", description: "Wrist bling",
-                           unit_price: 200, merchant_id: @merchant2.id)
-    @item_6 = Item.create!(name: "Necklace", description: "Neck bling",
-                           unit_price: 300, merchant_id: @merchant2.id)
+    @customer = Customer.create!(
+      first_name: "Joey",
+      last_name: "Smith"
+    )
 
-    @customer_1 = Customer.create!(first_name: "Joey", last_name: "Smith")
-    @customer_2 = Customer.create!(first_name: "Cecilia", last_name: "Jones")
-    @customer_3 = Customer.create!(first_name: "Mariah", last_name: "Carrey")
-    @customer_4 = Customer.create!(first_name: "Leigh Ann", last_name: "Bron")
-    @customer_5 = Customer.create!(first_name: "Sylvester", last_name: "Nader")
-    @customer_6 = Customer.create!(first_name: "Herber", last_name: "Kuhn")
+    @customer2 = Customer.create!(
+      first_name: "Billy",
+      last_name: "Jones"
+    )
 
-    @coupon1 = Coupon.create!(
+    @coupon = Coupon.create!(
       name: "TenPercentOff",
       code: "PO001",
       discount_type: "percentage",
       discount: 10,
       status: "activated",
-      merchant: @merchant1
+      merchant: @merchant
     )
 
-    @coupon2 = Coupon.create!(
-      name: "TenDollarsOff",
-      code: "DO001",
-      discount_type: "dollar",
-      discount: 1000,
-      status: "activated",
-      merchant: @merchant1
+    @invoice = @coupon.invoices.create!(
+      customer_id: @customer.id,
+      status: 2,
+      created_at: "2012-03-27 14:54:09"
     )
 
-    @invoice_1 = @coupon1.invoices.create!(customer_id: @customer_1.id,
-                                           status: 2, created_at: "2012-03-27 14:54:09")
-    @invoice_2 = @coupon1.invoices.create!(customer_id: @customer_1.id,
-                                           status: 2, created_at: "2012-03-28 14:54:09")
-    @invoice_3 = @coupon1.invoices.create!(customer_id: @customer_2.id,
-                                           status: 2)
-    @invoice_4 = @coupon1.invoices.create!(customer_id: @customer_3.id,
-                                           status: 2)
-    @invoice_5 = @coupon2.invoices.create!(customer_id: @customer_4.id,
-                                           status: 2)
-    @invoice_6 = @coupon2.invoices.create!(customer_id: @customer_5.id,
-                                           status: 2)
-    @invoice_7 = @coupon2.invoices.create!(customer_id: @customer_6.id,
-                                           status: 2)
-    @invoice_8 = @coupon2.invoices.create!(customer_id: @customer_6.id,
-                                           status: 1)
+    @invoice2 = Invoice.create!(
+      customer_id: @customer2.id,
+      status: 2
+    )
 
-    @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id,
-                                quantity: 9, unit_price: 10, status: 2)
-    @ii_2 = InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_1.id,
-                                quantity: 1, unit_price: 10, status: 2)
-    @ii_3 = InvoiceItem.create!(invoice_id: @invoice_3.id, item_id: @item_2.id,
-                                quantity: 2, unit_price: 8, status: 2)
-    @ii_4 = InvoiceItem.create!(invoice_id: @invoice_4.id, item_id: @item_3.id,
-                                quantity: 3, unit_price: 5, status: 1)
-    @ii_6 = InvoiceItem.create!(invoice_id: @invoice_5.id, item_id: @item_4.id,
-                                quantity: 1, unit_price: 1, status: 1)
-    @ii_7 = InvoiceItem.create!(invoice_id: @invoice_6.id, item_id: @item_7.id,
-                                quantity: 1, unit_price: 3, status: 1)
-    @ii_8 = InvoiceItem.create!(invoice_id: @invoice_7.id, item_id: @item_8.id,
-                                quantity: 1, unit_price: 5, status: 1)
-    @ii_9 = InvoiceItem.create!(invoice_id: @invoice_7.id, item_id: @item_4.id,
-                                quantity: 1, unit_price: 1, status: 1)
-    @ii_10 = InvoiceItem.create!(invoice_id: @invoice_8.id,
-                                 item_id: @item_5.id, quantity: 1, unit_price: 1, status: 1)
-    @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id,
-                                 item_id: @item_8.id, quantity: 12, unit_price: 6, status: 1)
+    @ii1 = InvoiceItem.create!(
+      invoice_id: @invoice.id,
+      item_id: @item1.id,
+      quantity: 9,
+      unit_price: @item1.unit_price,
+      status: 2
+    )
 
-    @transaction1 = Transaction.create!(credit_card_number: 203_942, result: 1,
-                                        invoice_id: @invoice_1.id)
-    @transaction2 = Transaction.create!(credit_card_number: 230_948, result: 1,
-                                        invoice_id: @invoice_2.id)
-    @transaction3 = Transaction.create!(credit_card_number: 234_092, result: 1,
-                                        invoice_id: @invoice_3.id)
-    @transaction4 = Transaction.create!(credit_card_number: 230_429, result: 1,
-                                        invoice_id: @invoice_4.id)
-    @transaction5 = Transaction.create!(credit_card_number: 102_938, result: 1,
-                                        invoice_id: @invoice_5.id)
-    @transaction6 = Transaction.create!(credit_card_number: 879_799, result: 0,
-                                        invoice_id: @invoice_6.id)
-    @transaction7 = Transaction.create!(credit_card_number: 203_942, result: 1,
-                                        invoice_id: @invoice_7.id)
-    @transaction8 = Transaction.create!(credit_card_number: 203_942, result: 1,
-                                        invoice_id: @invoice_8.id)
+    # 9000
+
+    @ii2 = InvoiceItem.create!(
+      invoice_id: @invoice.id,
+      item_id: @item2.id,
+      quantity: 10,
+      unit_price: @item2.unit_price,
+      status: 1
+    )
+
+    # 8000
+
+    @ii3 = InvoiceItem.create!(
+      invoice_id: @invoice.id,
+      item_id: @item3.id,
+      quantity: 10,
+      unit_price: @item3.unit_price,
+      status: 1
+    )
+
+    # 5000
+
+    @ii4 = InvoiceItem.create!(
+      invoice_id: @invoice2.id,
+      item_id: @item3.id,
+      quantity: 10,
+      unit_price: @item4.unit_price,
+      status: 1
+    )
+
+    @transaction = Transaction.create!(
+      credit_card_number: "000000000000",
+      result: 1,
+      invoice_id: @invoice.id
+    )
+
+    visit merchant_invoice_path(@merchant, @invoice)
   end
 
   it "shows the invoice information" do
-    visit merchant_invoice_path(@merchant1, @invoice_1)
-
-    expect(page).to have_content(@invoice_1.id)
-    expect(page).to have_content(@invoice_1.status)
-    expect(page).to have_content(@invoice_1.created_at.strftime("%A, %B %-d, %Y"))
+    expect(page).to have_content(@invoice.id)
+    expect(page).to have_content(@invoice.status)
+    expect(page).to have_content(@invoice.created_at.strftime("%A, %B %-d, %Y"))
   end
 
   it "shows the customer information" do
-    visit merchant_invoice_path(@merchant1, @invoice_1)
-
-    expect(page).to have_content(@customer_1.first_name)
-    expect(page).to have_content(@customer_1.last_name)
-    expect(page).to_not have_content(@customer_2.last_name)
+    expect(page).to have_content(@customer.first_name)
+    expect(page).to have_content(@customer.last_name)
+    expect(page).to_not have_content(@customer2.last_name)
   end
 
   it "shows the item information" do
-    visit merchant_invoice_path(@merchant1, @invoice_1)
-
-    expect(page).to have_content(@item_1.name)
-    expect(page).to have_content(@ii_1.quantity)
-    expect(page).to have_content(@ii_1.unit_price)
-    expect(page).to_not have_content(@ii_4.unit_price)
+    expect(page).to have_content(@item1.name)
+    expect(page).to have_content(@ii1.quantity)
+    expect(page).to have_content(@ii1.unit_price)
+    expect(page).to_not have_content(@ii4.unit_price)
   end
 
-  it "shows the total revenue for this invoice" do
-    visit merchant_invoice_path(@merchant1, @invoice_1)
-
-    expect(page).to have_content(@invoice_1.total_revenue)
+  it "shows the subtotal revenue for this invoice" do
+    expect(page).to have_content("$22,000")
   end
 
   it "shows a select field to update the invoice status" do
-    visit merchant_invoice_path(@merchant1, @invoice_1)
-
-    within("#the-status-#{@ii_1.id}") do
+    within("#the-status-#{@ii1.id}") do
       page.select("cancelled")
       click_button "Update Invoice"
 
@@ -148,5 +141,23 @@ RSpec.describe "invoices show" do
     within("#current-invoice-status") do
       expect(page).to_not have_content("in progress")
     end
+  end
+
+  it "shows the coupon used for this invoice and has a link to the coupon's show page" do
+    expect(page).to have_content("Coupon code: #{@coupon.code}")
+
+    click_link @coupon.name
+
+    expect(page).to have_current_path(merchant_coupon_path(@merchant, @coupon))
+  end
+
+  it "shows no coupon used if a coupon was not used" do
+    visit merchant_invoice_path(@merchant, @invoice2)
+
+    expect(page).to have_content("None")
+  end
+
+  it "shows the total revenue after the coupon was used" do
+    expect(page).to have_content(@invoice.total_revenue)
   end
 end
